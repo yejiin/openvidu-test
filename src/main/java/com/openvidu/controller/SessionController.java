e2710657fd97b8a30798903a601e9a506e8a743d
yejiin <yj_song67@naver.com>
2022-01-26 15:54:59 +0900

    refactor: getToken response 변경

--- src/main/java/com/openvidu/controller/SessionController.java
+++ src/main/java/com/openvidu/controller/SessionController.java
@@ -1,15 +1,14 @@
 package com.openvidu.controller;
 
 import com.openvidu.dto.GetTokenReq;
-import com.openvidu.dto.GetTokenRes;
 import com.openvidu.dto.RemoveUserReq;
 import com.openvidu.repository.MemberRepository;
 import io.openvidu.java.client.*;
-import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -64,7 +63,6 @@ public class SessionController {
         // Role associated to this user
         OpenViduRole role = memberRepository.find((String) httpSession.getAttribute("loggedUser")).getRole();
 
-
         // Optional data to be passed to other users then this user connects to the video-call. In this case, a JSON with the value we stored in the HttpSession object on Login
         String serverData = "{\"serverData\": \"" + httpSession.getAttribute("loggedUser") + "\"}";
 
@@ -74,6 +72,8 @@ public class SessionController {
                 .data(serverData)
                 .role(role)
                 .build();
+
+        JSONObject responseJson = new JSONObject();
 
         if (this.mapSessions.get(sessionName) != null) {
             // Session already exists
@@ -86,8 +86,11 @@ public class SessionController {
                 // Update our collection storing the new token
                 this.mapSessionNamesTokens.get(sessionName).put(token, role);
 
+                // Prepare the response with the token
+                responseJson.put(0, token);
+
                 // Return the response to the client
-                return ResponseEntity.ok(new GetTokenRes(token));
+                return new ResponseEntity<>(responseJson, HttpStatus.OK);
             } catch (OpenViduJavaClientException e) {
                 // If internal error generate an error message and return it to client
                 return getErrorResponse(e);
@@ -114,8 +117,11 @@ public class SessionController {
             this.mapSessionNamesTokens.put(sessionName, new ConcurrentHashMap<>());
             this.mapSessionNamesTokens.get(sessionName).put(token, role);
 
+            // Prepare the response with the token
+            responseJson.put(0, token);
+
             // Return the response to the client
-            return ResponseEntity.ok(new GetTokenRes(token));
+            return new ResponseEntity<>(responseJson, HttpStatus.OK);
         } catch (Exception e) {
             // If error generate an error message and return it to client
             return getErrorResponse(e);
